7a1cd47c794f4411cec3fcd847e693985e18220a
Melissa F <melissa.faulner@gmail.com>
2020-10-06 16:06:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
+  input_to_index(user_input)
   move(board, index, value = "X")
   valid_move?(board, index)
   if valid_move? == TRUE
