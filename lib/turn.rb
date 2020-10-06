5a9edccc52aa91e4833e356d0dae72ebbc938bc6
Melissa F <melissa.faulner@gmail.com>
2020-10-06 16:05:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
@@ -14,13 +22,6 @@ def turn(board)
   end
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
 def input_to_index(user_input)
   index = "#{user_input}".to_i - 1
